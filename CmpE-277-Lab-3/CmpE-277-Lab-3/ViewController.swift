ab9db4a5dd3eb74d3f1a56836b1dc0b3edc02e25
Jelson Santos <jesantos0527@gmail.com>
2016-12-12 00:02:34 -0800

    initial landing screen

--- CmpE-277-Lab-3/CmpE-277-Lab-3/ViewController.swift
+++ CmpE-277-Lab-3/CmpE-277-Lab-3/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var addButton: UIButton!
+    @IBOutlet var subtractionButton: UIButton!
+    @IBOutlet var multiplicationButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func additionOperation(sender: UIButton) {
+        print("Addition button was pressed")
+    }
 
+    @IBAction func subtractionOperation(sender: UIButton) {
+        print("Subtraction button was pressed")
+    }
+
+    @IBAction func multiplicationQuizOperation(sender: UIButton) {
+        print("multiplication was pressed")
+    }
+    
+    
 }
 
